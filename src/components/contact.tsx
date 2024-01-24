59400913b973982dd4b0c4c71d6ad5913d543ba8
marciosaraiva7 <marciohm7@gmail.com>
2024-01-24 17:05:14 -0300

    new updates

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -44,15 +44,17 @@ export default function Contact() {
           backgroundImage: "linear-gradient(to bottom, #6992E0, #08205E)",
         }}
       >
-        <div className="relative z-20 mb-[1rem] flex w-full flex-col items-center rounded-[40px] bg-white p-[1rem]">
+        <div className="relative z-20 mb-[1rem] flex w-full flex-col items-center rounded-[40px] bg-white p-[2rem]">
           {!check && (
             <p className="mb-[1rem] text-center text-[1.6rem] font-bold text-[#1D4D9D] md:text-[1.75rem]">
-              Deixe-nos saber como podemos ajudar
+              Fale com um especialista!
             </p>
           )}
           {!check && (
-            <p className="mb-[3rem] text-center text-[1.4rem] leading-8 text-[#1D4D9D] md:w-[30rem] md:text-[1.75rem]">
-              Preencha o formulário abaixo para uma consulta gratuita{" "}
+            <p className="mb-[3rem] text-center text-[1.4rem] leading-8 text-[#1D4D9D] md:w-[65%] md:text-[1.30rem]">
+              Entre em contato conosco para saber mais sobre como podemos
+              ajudá-lo a desenvolver aplicações digitais de sucesso para o seu
+              negócio.
             </p>
           )}
           {!check && (
