d57f08f7eb73c8344f968a08580f6bd5ea9d90f7
marciosaraiva7 <marciohm7@gmail.com>
2024-04-12 13:04:19 -0300

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import "../../styles/fonts.css";
 import Header from "@/components/header";
-import FloatButton from "@/components/floatButton";
+import { GoogleTagManager } from "@next/third-parties/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Neptune Lab",
@@ -16,10 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt" className="scroll-smooth">
-      <body>
+      <head>
+        <GoogleTagManager gtmId="GTM-M3R82KK5" />
+      </head>
+      <body className={inter.className}>
         <Header />
         {children}
-        <FloatButton />
       </body>
     </html>
   );
