09e35e3725b6ea9e049eba7604ca336bd7df6f4e
marciosaraiva7 <marciohm7@gmail.com>
2024-04-12 14:25:39 -0300

    update form

--- src/components/ui/button-loading.tsx
+++ src/components/ui/button-loading.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 // Define a type for your component's props
 type ButtonLoadingProps = {
   isLoading: boolean;
+  isFormFilled:boolean;
   children: ReactNode;
   className?: string; // Optional property
 } & ButtonHTMLAttributes<HTMLButtonElement>;
@@ -12,10 +13,11 @@ type ButtonLoadingProps = {
 export function ButtonLoading({
   isLoading,
   children,
+  isFormFilled,
   className,
 }: ButtonLoadingProps) {
   return (
-    <Button disabled={isLoading} className={className}>
+    <Button disabled={!isFormFilled || isLoading} className={className}>
       {isLoading && <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />}
       {children}
     </Button>
