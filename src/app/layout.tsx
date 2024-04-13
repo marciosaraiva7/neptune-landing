314750dce104da773567b116db50b29c574fb46d
marciosaraiva7 <marciohm7@gmail.com>
2024-04-13 13:32:30 -0300

    new description and SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/next-script-for-ga */
 import type { Metadata } from "next";
+import Head from "next/head";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
@@ -8,10 +9,10 @@ import FloatButton from "@/components/floatButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Neptune Lab",
-  description: "Consultoria e desenvolvimento digital.",
-};
+// export const metadata: Metadata = {
+//   title: "Neptune Lab",
+//   description: "Consultoria e desenvolvimento digital.",
+// };
 
 export default function RootLayout({
   children,
@@ -20,9 +21,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt" className="scroll-smooth">
-      <head>
+      <Head>
+        <title>Neptune Lab</title>
+        <meta
+          name="description"
+          content="Neptune Lab é especializada no desenvolvimento de aplicativos, sistemas customizados, sites, plataformas de e-commerce e dashboards interativos. Descubra como nossas soluções tecnológicas personalizadas podem transformar sua operação digital e otimizar seus processos de negócio. Conecte-se com as soluções ideais para crescer seu negocio com um ambiente digital moderno."
+          key={"desc"}
+        />
+        <meta
+          property="og:title"
+          content="Neptune Lab | Desenvolvimento de apps e sistemas"
+        />
+        <meta
+          property="og:description"
+          content="Neptune Lab é especializada no desenvolvimento de aplicativos, sistemas customizados, sites, plataformas de e-commerce e dashboards interativos. Descubra como nossas soluções tecnológicas personalizadas podem transformar sua operação digital e otimizar seus processos de negócio. Conecte-se com as soluções ideais para crescer seu negocio com um ambiente digital moderno."
+        />
+        <meta property="og:image" content="/images/favicon.ico" />
         <GoogleTagManager gtmId="GTM-M3R82KK5" />
-      </head>
+      </Head>
       <body className={inter.className}>
         <Header />
         {children}
